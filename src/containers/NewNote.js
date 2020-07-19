62760fb0d1c6f253de7b3324c658f8c27574643f
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2020-07-19 16:15:35 +0100

    pushing attachment to s3

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -5,6 +5,7 @@ import { API } from "aws-amplify";
 
 import LoaderButton from "../components/LoaderButton";
 import { onError } from "../libs/errorLib";
+import { s3Upload } from "../libs/awsLib";
 import config from "../config";
 import "./NewNote.css";
 
@@ -27,8 +28,9 @@ export default function NewNote() {
 
         if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
             alert(
-                `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
-                1000000} MB.`
+                `Please pick a file smaller than ${
+                    config.MAX_ATTACHMENT_SIZE / 1000000
+                } MB.`
             );
             return;
         }
@@ -36,7 +38,9 @@ export default function NewNote() {
         setIsLoading(true);
 
         try {
-            await createNote({ content });
+            const attachment = file.current ? await s3Upload(file.current) : null;
+
+            await createNote({ content, attachment });
             history.push("/");
         } catch (e) {
             onError(e);
