fd487fdcca8d4e0b353cfe78f73cd17024fe3831
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2020-07-19 12:17:26 +0100

    added aws amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-upload-34sadf47xg"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://7h14p86db6.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_WLkCom1Oz",
+        APP_CLIENT_ID: "2ghet9g653o4u3m46g4vi8mqfu",
+        IDENTITY_POOL_ID: "us-east-1:12f5fb0e-9a97-4ec9-a74c-1f2c66be1f4d"
+    }
+};
