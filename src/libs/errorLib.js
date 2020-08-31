b87ee10748c1d42399f6817192fe7ea5c2893127
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2020-08-31 16:25:05 +0100

    install sentry

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,3 +1,32 @@
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+    if (isLocal) {
+        return;
+    }
+
+    Sentry.init({
+        dsn: "https://582e65b9eb8d41a0ab2dcf6fe28fc851@o441655.ingest.sentry.io/5412145",
+        integrations: [
+            new Integrations.BrowserTracing(),
+        ],
+        tracesSampleRate: 1.0,
+    });
+}
+
+export function logError(error, errorInfo = null) {
+    if (isLocal) {
+        return;
+    }
+
+    Sentry.withScope((scope) => {
+        errorInfo && scope.setExtras(errorInfo);
+        Sentry.captureException(error);
+    });
+}
+
 export function onError(error) {
     let message = error.toString();
 
