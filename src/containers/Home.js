615d5137f94b3f1db614eeb19da3b8506af17222
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2020-08-31 16:05:53 +0100

    Updating lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,6 +6,7 @@ import { LinkContainer } from "react-router-bootstrap";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import "./Home.css";
+import {Link} from "react-router-dom";
 
 
 export default function Home() {
@@ -61,6 +62,14 @@ export default function Home() {
             <div className="lander">
                 <h1>Scratch</h1>
                 <p>A simple note taking app</p>
+                <div>
+                    <Link to="/login" className="btn btn-info btn-lg">
+                        Login
+                    </Link>
+                    <Link to="/signup" className="btn btn-success btn-lg">
+                        Signup
+                    </Link>
+                </div>
             </div>
         );
     }
